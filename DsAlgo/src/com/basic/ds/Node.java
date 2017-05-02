2865289108467ce59e88f66b0767d6c9eca9463f
keyuls <keyul@asu.edu>
2017-05-02 08:16:13 -0700

    Node structure

---
+++ DsAlgo/src/com/basic/ds/Node.java
@@ -0,0 +1,21 @@
+package com.basic.ds;
+
+public class Node {
+	private Node node;
+	private String data;
+	
+	public void addNode(){
+		
+	}
+	
+	public void deleteNode(){
+		
+	}
+	
+	public void searchNode(){
+		
+	}
+}
+
+
+
